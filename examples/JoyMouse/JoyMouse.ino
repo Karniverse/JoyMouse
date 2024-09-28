dde76388a9f2ae735ece488c3eb9c9920571abbd
mukil-bluerider <mrmukil1991@gmail.com>
2024-09-28 19:30:05 +0530

    Update

--- examples/JoyMouse/JoyMouse.ino
+++ examples/JoyMouse/JoyMouse.ino
@@ -1,14 +1,14 @@
-//#include <JoyMouse.h>
+// #include <JoyMouse.h>
 #include "JoyMouse.h"
 
-
-
-void setup() {
-    // Initialize joystick with X and Y axis analog pins (A0 and A1 in this case)
+void setup()
+{
+    // Initialize joystick with X and Y axis analog pins (A7 and A6 in this case)
     initJoystickMouse(A7, A6);
 }
 
-void loop() {
+void loop()
+{
     // Update mouse movement based on joystick input
     updateJoystickMouse();
 }
